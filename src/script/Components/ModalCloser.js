66f0acb0f4e4ff4433b05d37292dacab4360b6cc
FilipKrz03 <122054858+FilipKrz03@users.noreply.github.com>
2023-04-04 23:57:47 +0200

    Add files via upload

--- src/script/Components/ModalCloser.js
+++ src/script/Components/ModalCloser.js
@@ -1,1 +1,9 @@
+export class ModalCloser {
 
+    static closeModal(modal){
+        const modalToClose = document.querySelector(modal)
+        const overlay = document.getElementById('overlay');
+        modalToClose.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+}
