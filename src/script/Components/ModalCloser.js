b5284fbc6d74e96b3d70ceaeec25e45033f64adf
FilipKrz03 <122054858+FilipKrz03@users.noreply.github.com>
2023-04-07 14:24:00 +0200

    Delete ModalCloser.js

--- src/script/Components/ModalCloser.js
+++
@@ -1,9 +0,0 @@
-export class ModalCloser {
-
-    static closeModal(modal){
-        const modalToClose = document.querySelector(modal)
-        const overlay = document.getElementById('overlay');
-        modalToClose.classList.remove('active');
-        overlay.classList.remove('active');
-    }
-}
